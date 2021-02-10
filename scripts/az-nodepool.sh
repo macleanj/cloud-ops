76f06fcbc70f4d16e498174ace2c01171371ac10
Jerome Mac Lean <jerome@mac-lean.com>
2021-02-10 09:33:02 +0100

    Adding gitlab scripts

--- scripts/az-nodepool.sh
+++ scripts/az-nodepool.sh
@@ -1,7 +1,7 @@
 # Scripts used for the most common Azure operational tasks
 resourceGroup=K8S_WEU_DEV_3.0_1
 clusterName=aks-mgt-k8s-dev
-scaleDynamics=6 # 2 times the amount of zones to be sure the hostname with the availability zone is available for the nodeSelectorTerms
+scaleDynamics=6 # Added/Deleted amount of notes compared to the current count.
 nodePoolName=infra
 
 # Set nodepool min-count to current count +5 to enable multi-zone setup"
@@ -11,12 +11,12 @@ export currentMaxCount=$(echo $nodepoolShow | jq -r '.maxCount')
 echo "Current nodeCount=$currentCount"
 export newMinCount=$(($currentCount+$scaleDynamics))
 echo "New minCount=$newMinCount / maxCount=$currentMaxCount"
-# az aks nodepool update --update-cluster-autoscaler --min-count $newMinCount --max-count $currentMaxCount --cluster-name "$clusterName" --name $nodePoolName --resource-group "$resourceGroup"
 az aks nodepool update --disable-cluster-autoscaler --cluster-name "$clusterName" --name $nodePoolName --resource-group "$resourceGroup"
 az aks nodepool scale --node-count $newMinCount --no-wait --cluster-name "$clusterName" --name $nodePoolName --resource-group "$resourceGroup"
 
 # Set nodepool min-count to current -5 nodes to nodepool to enable multi-zone setup"
 export currentMaxCount=30
-export newMinCount=1
+export newMinCount=3
 echo "New minCount=$newMinCount / maxCount=$currentMaxCount"
 az aks nodepool update --enable-cluster-autoscaler --min-count $newMinCount --max-count $currentMaxCount --cluster-name "$clusterName" --name $nodePoolName --resource-group "$resourceGroup"
+# az aks nodepool update --update-cluster-autoscaler --min-count $newMinCount --max-count $currentMaxCount --cluster-name "$clusterName" --name $nodePoolName --resource-group "$resourceGroup"
---
+++ scripts/gitlab.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Scripts used for the most common GitLab operational tasks
+
+PROGRAM=$(basename "$0")
+BASENAME=$(echo $(basename "$0") | sed -e 's/.sh//g')
+[ -z $CI_SERVER_URL ] && CI_SERVER_URL="git.eon-cds.de"
+[ -z $CI_PROJECT_ID ] && CI_PROJECT_ID="5892"
+[ -z $CI_COMMIT_BRANCH ] && CI_COMMIT_BRANCH="develop"
+
+read -r -d '' layers <<- EOM
+02-vault
+09-networking
+10-k8s-cluster
+11-k8s-infrastructure
+15-k8s-app-gw-ingress
+19-k8s-network-policies
+21-k8s-opa-policy-templates
+22-k8s-opa-policies
+30-k8s-kured
+50-k8s-storage
+EOM
+
+Usage()
+{
+  echo ""
+  echo "Usage: $PROGRAM [-trigger <values>]"
+  echo ""
+  echo "options:"
+  echo "-trigger  : Trigger CI pipeline with variables."
+  echo "-server   : GitLab Server. Default: $CI_SERVER_URL"
+  echo "-project  : GitLab Project. Default: $CI_PROJECT_ID"
+  echo "-ref      : GitLab Branch. Default: $CI_COMMIT_BRANCH"
+  echo ""
+  echo "trigger:"
+  echo "values    : variables[<key>]=<value>"
+  echo "$PROGRAM -trigger variables[REPO_DIR]=02-vault"
+  exit 5
+}
+
+
+# Read and/or validate command-line parameters
+ReadParams()
+{
+	[ $# -eq 0 ] && Usage
+
+	while [ $# -gt 0 ]
+	do
+		case ${1}	in
+		"-trigger")
+      if [[ ${2} =~ "^-" || -z ${2} ]]; then
+        triggerValues="all";
+        shift 1
+      else
+        triggerValues="${2}"
+        shift 2
+      fi
+			;;
+		"-server")
+      [[ ${2} =~ "^-" || -z ${2} ]]  && Usage || CI_SERVER_URL="${2}"
+      shift 2
+      ;;
+		"-project")
+      [[ ${2} =~ "^-" || -z ${2} ]] && Usage || CI_PROJECT_ID="${2}"
+      shift 2
+      ;;
+		"-ref")
+      [[ ${2} =~ "^-" || -z ${2} ]] && Usage || CI_COMMIT_BRANCH="${2}"
+      shift 2
+      ;;
+    "-debug")
+      debug=1
+      shift 1
+      ;;
+		*)
+			echo ">>>>> Wrong argument given: ${1}"; Usage
+			;;
+		esac
+ 	done
+
+  # Obligated fields
+  [ -z $triggerValues ] && echo "No option(s) given. Exitting....." && Usage
+}
+
+##########################
+# Main script
+##########################
+ReadParams "$@"
+
+if [ ! -z $triggerValues ]; then
+  [ -z $GITLAB_CI_TRIGGER_TOKEN ] && echo "Missing \$GITLAB_CI_TRIGGER_TOKEN. Exitting....." && exit 5
+
+  echo "CI_SERVER_URL      : $CI_SERVER_URL"
+  echo "CI_PROJECT_ID      : $CI_PROJECT_ID"
+  echo "CI_COMMIT_BRANCH   : $CI_COMMIT_BRANCH"
+
+  if [[ "$triggerValues" == "all" ]]; then
+    for layer in $layers; do
+      echo "Triggering layer $layer"
+      curl --request POST \
+        --form "token=$GITLAB_CI_TRIGGER_TOKEN" \
+        --form "ref=$CI_COMMIT_BRANCH" \
+        --form "variables[REPO_DIR]=$layer" \
+        "https://$CI_SERVER_URL/api/v4/projects/$CI_PROJECT_ID/trigger/pipeline"
+      echo ""
+      sleep 5
+    done
+  else
+    echo "Triggering pipeline "
+    curl --request POST \
+      --form "token=$GITLAB_CI_TRIGGER_TOKEN" \
+      --form "ref=$CI_COMMIT_BRANCH" \
+      --form "$triggerValues" \
+      "https://$CI_SERVER_URL/api/v4/projects/$CI_PROJECT_ID/trigger/pipeline"
+      echo ""
+  fi
+else
+  echo "Unknown options given"
+fi
