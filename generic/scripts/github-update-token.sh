ec21579ca8a28df93d3f2d83c6a2543a672e79ff
Jerome Mac Lean <jerome@mac-lean.com>
2021-06-16 08:45:31 +0200

    Adding gitlab

--- generic/scripts/github-update-token.sh
+++ generic/scripts/github-update-token.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-rm ~/Library/Application\ Support/SourceTree/jmaclean@STAuth-github.com
+# rm ~/Library/Application\ Support/SourceTree/jmaclean@STAuth-github.com
+# rm ~/Library/Application\ Support/SourceTree/jerome@mac-lean.com@STAuth-gitlab.com
+
+# Alternatively some mentionings
 # git credential-osxkeychain erase
