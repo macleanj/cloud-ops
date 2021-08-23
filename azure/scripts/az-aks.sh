e00582f5fb5575443022b25cdc1490f71f910196
Jerome Mac Lean <jerome@mac-lean.com>
2021-08-23 10:47:31 +0200

    AKS UptimeSLA and upgrade enhancements.

--- azure/scripts/az-aks.sh
+++ azure/scripts/az-aks.sh
@@ -52,7 +52,7 @@ az aks nodepool list --cluster-name $clusterName --resource-group $resourceGroup
   '
 
 # Upgrade Cluster
-newK8SVerion="1.20.5"
+newK8SVerion="1.21.2"
 az aks upgrade --resource-group $resourceGroup --name $clusterName --control-plane-only --kubernetes-version $newK8SVerion
 az aks show --resource-group $resourceGroup --name $clusterName --output table
 
@@ -113,3 +113,27 @@ kubectl delete node $nodeName # Also delete from vss
 #   kubectl delete -n $namespace pod $pod
 # done
 # IFS=$OLDIFS
+
+# Remove cluster
+# az aks delete --name $clusterName --resource-group $resourceGroup
+
+
+## Change SKU of cluster between Free and UptimeSLA
+# https://docs.microsoft.com/en-us/azure/aks/uptime-sla#modify-an-existing-cluster-to-use-uptime-sla
+# https://docs.microsoft.com/en-us/azure/aks/uptime-sla#opt-out-of-uptime-sla
+az aks update --resource-group $resourceGroup --name $clusterName --uptime-sla
+az aks update --resource-group $resourceGroup --name $clusterName --no-uptime-sla
+az aks show --name $clusterName --resource-group $resourceGroup |  jq -r '.sku'
+
+# Load balancer configuration
+# With regard to the SNAT exhaustion issue: "Load balancer profile allocated ports is not in an allowable range given the number of nodes and IPs provisioned. Refer to https://aka.ms/aks/slb-ports for more details."
+# Calculation:
+# Total nodes: 5 x nodepool of 30 + 3 x system = 153 nodes (when auto-scaling this is the max value)
+# 4000 ports per node: 612000 slb ports
+# IP addressed needed: 612000 / 64000 = 10
+# Same adding maxSurge
+# Total nodes: 153 * 33% = 204 nodes
+# 4000 ports per node: 813960 slb ports
+# IP addressed needed: 813960 / 64000 = 13
+aksResourceGroup=$(az aks show --resource-group $resourceGroup --name $clusterName --query nodeResourceGroup -o tsv)
+az network lb outbound-rule list --resource-group $aksResourceGroup --lb-name kubernetes -o table
